69cccc1152215a0c04fd3aae11d4b840f2fe7922
Pooja C <poojachandran5527@gmail.com>
2021-10-11 16:48:49 +0530

    division

--- test.js
+++ test.js
@@ -6,8 +6,11 @@ function Sum(a, b){
 function Sub(a, b){
     return a - b;
 }
-
-
+function Div(a, b){
+    return a / b;
+}
+const div = Div(6, 3);
+console.log(div)
 const sum = Sum(3, 4);
 console.log(sum)
 const sub = Sum(3, 5);
